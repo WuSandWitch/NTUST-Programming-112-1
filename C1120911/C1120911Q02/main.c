f0bf6e3437bc8ec4f67e61f3c0952d434b1e8511
WuSandWitch <wusandwitch@gmail.com>
2023-09-11 11:37:36 +0800

    Upload 9/11

---
+++ C1120911/C1120911Q02/main.c
@@ -0,0 +1,15 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	float a, b;
+	scanf("%f",&a);
+	scanf("%f",&b);
+
+	printf("%.2f + %.2f = %.2f\n", a, b, a + b);
+	printf("%.2f - %.2f = %.2f\n", a, b, a - b);
+	printf("%.2f * %.2f = %.2f\n", a, b, a * b);
+	printf("%.2f / %.2f = %.2f\n", a, b, a / b);
+}
---
+++ C1120911/C1120911Q03/main.c
@@ -0,0 +1,12 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	char operator;
+
+	scanf("%d %c %d", &a, &operator,&b);
+	printf("%d %c %d", a, operator,b);
+}
