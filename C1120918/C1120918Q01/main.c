90afc1f8af27115464bb960e51d4cd0a482a7f0f
WuSandWitch <wusandwitch@gmail.com>
2023-09-18 09:18:35 +0800

    Upload second class quiz

---
+++ C1120918/C1120918Q01/main.c
@@ -0,0 +1,14 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float a, b;
+	scanf("%f %f", &a, &b);
+	float length = sqrt(pow(a, 2) + pow(b, 2));
+
+	printf("%.2f\n", length);
+	printf("%.2f %.2f", a / length, b / length);
+}
---
+++ C1120918/C1120918Q02/main.c
@@ -0,0 +1,14 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	float x1, y1, x2, y2;
+	scanf("%f %f", &x1, &y1);
+	scanf("%f %f", &x2, &y2);
+
+	printf("%.2f %.2f\n", x1 + x2, y1 + y2);
+	printf("%.2f %.2f\n", x1 - x2, y1 - y2);
+	printf("%.2f", x1 * x2 + y1 * y2);
+}
