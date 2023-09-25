8d4773a81c994883dd5c5b75646f18cb3eea68e5
WuSandWitch <wusandwitch@gmail.com>
2023-09-25 10:09:27 +0800

    Upload quiz 3

---
+++ C1120925/C1120925Q01/main.c
@@ -0,0 +1,27 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	float f_a, f_b;
+	char opr;
+	scanf("%f%c%f", &f_a,&opr,&f_b);
+	float result = 0;
+
+	if (opr == '+') {
+		result = f_a + f_b;
+	}
+	else if (opr == '-') {
+		result = f_a - f_b;
+	}
+	else if (opr == '*') {
+		result = f_a * f_b;
+	}
+	else if (opr == '/') {
+		result = f_a/f_b;
+	}
+
+	printf("%.2f%c%.2f=%.2f", f_a,opr,f_b,result);
+	return 0;
+}
---
+++ C1120925/C1120925Q02/main.c
@@ -0,0 +1,17 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		char sym = 'A' + i;
+
+		for (int j = 0; j < n - i - 1; j++) printf(" ");
+		for (int j = 0; j < (i + 1) * 2 - 1; j++)printf("%c", sym);
+
+		printf("\n");
+	}
+}
