0fb560960e405d6846014d11b0e38e60d8acdbf5
WuSandWitch <wusandwitch@gmail.com>
2023-10-02 16:31:12 +0800

    Upload 1002

---
+++ C1121002/C1121002Q01/main.c
@@ -0,0 +1,23 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	int cnt = 1;
+	char current;
+
+	while (1) {
+		scanf("%c", &current);
+		if (current == '\0' || current == '\n' || current == EOF) {
+			break;
+		}
+		if (current == ' ') {
+			cnt += 1;
+			printf("\n");
+			continue;
+		}
+		printf("%c", current);
+	}
+	printf("\n%d", cnt);
+}
---
+++ C1121002/C1121002Q02/main.c
@@ -0,0 +1,28 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	getchar();
+	char current;
+	int cnt;
+
+
+	for (int i = 0; i < n; i++) {
+		printf("Case %d: ", i + 1);
+		
+		while(1) {
+			current = getchar();
+			if (current == '\n') break;
+			scanf("%d",&cnt);
+			
+			for (int j = 0; j < cnt; j++) {
+				printf("%c", current);
+			}
+		}
+		printf("\n");
+	}
+}
