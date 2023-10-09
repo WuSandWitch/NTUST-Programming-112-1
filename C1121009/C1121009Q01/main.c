cadc4cd11c40996f19d34f3bc3acdacf9d1f5905
WuSandWitch <wusandwitch@gmail.com>
2023-10-09 22:08:08 +0800

    Upload 1009

---
+++ C1121009/C1121009Q01/main.c
@@ -0,0 +1,28 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+	char num[1001];
+	scanf("%s", num);
+
+	int odd_sum = 0, even_sum = 0;
+
+	for (int i = 0; i < strlen(num); i++) {
+		if (i % 2 == 0) {
+			odd_sum += num[i] - '0';
+		}
+		else {
+			even_sum += num[i] - '0';
+		}
+	}
+	if (abs(odd_sum - even_sum) % 11) {
+		printf("%s is not a multiple of 11.", num);
+	}
+	else {
+		printf("%s is a multiple of 11.", num);
+	}
+}
---
+++ C1121009/C1121009Q02/main.c
@@ -0,0 +1,29 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d\n", &n);
+
+
+	for (int i = 0; i < n; i++) {
+		char current;
+		int sum = 0;
+		int current_sum = 0;
+		while (1) {
+			scanf("%c", &current);
+			if (current == '\n') break;
+
+			if (current == 'O') {
+				current_sum += 1;
+				sum += current_sum;
+			}
+			else {
+				current_sum = 0;
+			}
+		}
+		printf("%d\n", sum);
+	}
+}
