53ad0fd71c5633fdbe334686414c6e6413eaf5d9
WuSandWitch <wusandwitch@gmail.com>
2023-11-20 18:44:29 +0800

    Upload 1120

---
+++ C1121120/C1121120Q01/main.c
@@ -0,0 +1,91 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+#include <stdbool.h>
+
+void move(int* x, int *y, int destination) {
+	if (destination == 0) {
+		*y -= 1;
+	}
+	else if (destination == 1) {
+		*x += 1;
+	}
+	else if (destination == 2) {
+		*y += 1;
+	}
+	else if (destination == 3) {
+		*x -= 1;
+	}
+}
+
+char next(char maze[15][15], int x, int y, int destination) {
+	if (destination == 0) {
+		return maze[y - 1][x];
+	}
+	else if (destination == 1) {
+		return maze[y][x + 1];
+	}
+	else if (destination == 2) {
+		return maze[y + 1][x];
+	}
+	else if(destination == 3) {
+		return maze[y][x - 1];
+	}
+}
+
+
+int main()
+{
+	char maze[15][15];
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 15; j++) {
+			scanf("%c", &maze[i][j]);
+		}
+		if(i != 14) scanf("\n");
+	}
+	bool has_exit = false;
+	int entry_y, entry_x;
+	int exit_y, exit_x;
+	for (int i = 0; i < 15; i++) {
+		if (maze[i][14] == 'X') {
+			has_exit = true;
+			exit_x = 14;
+			exit_y = i;
+		}
+		if (maze[i][0] == 'X') {
+			entry_x = 0;
+			entry_y = i;
+		}
+	}
+	if (!has_exit) {
+		printf("This maze has no solution");
+		return;
+	}
+
+	int y = entry_y, x = entry_x;
+	int destination = 1; // 0:up, 1:right, 2:down, 3:left
+	while (!(x == exit_x && y == exit_y)) {
+		//printf("(%d,%d)\n", x, y);
+		if (next(maze, x, y, (destination + 1) % 4) == 'X') {
+			destination = (destination + 1) % 4;
+			move(&x, &y, destination);
+		}
+		else if (next(maze, x, y, destination) == 'X') {
+			move(&x, &y, destination);
+		}
+		else if (next(maze, x, y, (destination + 3) % 4) == 'X') {
+			destination = (destination + 3) % 4;
+			move(&x, &y, destination);
+		}
+		else if (next(maze, x, y, (destination + 2) % 4) == 'X') {
+			destination = (destination + 2) % 4;
+			move(&x, &y, destination);
+		}
+
+		if (x == entry_x && y == entry_y) {
+			printf("This maze has no solution");
+			return;
+		}
+	}
+	printf("This maze has a solution");
+}
---
+++ C1121120/C1121120Q02/main.c
@@ -0,0 +1,73 @@
+﻿#pragma warning(disable : 4996)
+#pragma warning(disable : 6031)
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int m, n;
+	
+	int cnt = 1;
+	while (1) {
+		scanf("%d %d\n", &m, &n);
+		if (m == 0 && n == 0) {
+			return;
+		}
+		int* map = malloc(m * n * sizeof(int));
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				map[i * n + j] = 0;
+			}
+		}
+		char current;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				scanf("%c", &current);
+				if (current == '*') {
+					map[i * n + j] = -1;
+
+					if (i != 0) {
+						map[(i - 1) * n + j] += (map[(i - 1) * n + j] != -1) ? 1 : 0;
+					}
+					if (i != m - 1) {
+						map[(i + 1) * n + j] += (map[(i + 1) * n + j] != -1) ? 1 : 0;
+					}
+					if (j != 0) {
+						map[i * n + j - 1] += (map[i * n + j - 1] != -1) ? 1 : 0;
+					}
+					if (j != n - 1) {
+						map[i * n + j + 1] += (map[i * n + j + 1] != -1) ? 1 : 0;
+					}
+					if (i != 0 && j != 0) {
+						map[(i - 1) * n + j - 1] += (map[(i - 1) * n + j - 1] != -1) ? 1 : 0;
+					}
+					if (i != 0 && j != n - 1) {
+						map[(i - 1) * n + j + 1] += (map[(i - 1) * n + j + 1] != -1) ? 1 : 0;
+					}
+					if (i != m - 1 && j != n - 1) {
+						map[(i + 1) * n + j + 1] += (map[(i + 1) * n + j + 1] != -1) ? 1 : 0;
+					}
+					if (i != m - 1 && j != 0) {
+						map[(i + 1) * n + j - 1] += (map[(i + 1) * n + j - 1] != -1) ? 1 : 0;
+					}
+				}
+			}
+			if (i != m -1) scanf("\n");
+		}
+		printf("Field #%d:\n", cnt);
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (map[i * n + j] == -1) {
+					printf("*");
+				}
+				else {
+					printf("%d", map[i * n + j]);
+				}
+			}
+			printf("\n");
+		}
+		printf("\n");
+		cnt++;
+	}
+}
